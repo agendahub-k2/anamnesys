1d71a072e5bbe88e36192fb5a556b10381ebd4cb
italok2 <agendahub.service@gmail.com>
2024-10-31 14:18:04 -0300

    setup inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import './App.css'; // Se você tiver um CSS global
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div style={{ marginTop: '64px' }}> {/* Ajuste a margem conforme necessário */}
+        <Routes>
+          {/* <Route path="/" element={<Login />} /> */}
+          {/* Outras rotas podem ser adicionadas aqui */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
